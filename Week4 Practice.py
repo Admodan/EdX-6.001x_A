ad1dd71b8fe3d697f54679e0ec5b1cc7c3d245e4
Adam Yars <adamyars@outlook.com>
2018-02-12 17:35:25 -0700

    satisfiesF()

--- Week4 Practice.py
+++ Week4 Practice.py
@@ -1,11 +1,19 @@
-def nfruits(fruitBag, roadFruit):
-    assert not len(fruitBag) == 0, 'No fruit in the bag'
-    assert not len(roadFruit) == 0, 'No road fruit eaten'
-    for letter in roadFruit:
-        if letter in fruitBag.keys():
-            fruitBag[letter] -= 1
-    for key in fruitBag.keys():
-        if key not in roadFruit:
-            fruitBag[key] += 1
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements
+    Returns the length of L after mutation
+    """
+    trueList = []
+    for item in L:
+        if f(item) == True:
+            trueList.append(item)
+    L = trueList
+    return len(L)
 
-    return max(fruitBag.values())
+#L = ['ass', 'hat', 'baoot', 'squeeze', 'squish', 'affirmation', '', 'nope', 'n', 'a']
+
+def f(s):
+    return 'a' in s
