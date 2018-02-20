bacbb6d15408268955ccc96556e82ee58dbd5d15
Adam Yars <adamyars@outlook.com>
2018-02-20 06:38:37 -0700

    Added Lec10, Lec11, and finger_exercises code files.

---
+++ Lec10Code.py
@@ -0,0 +1,18 @@
+#import string
+#def hash(s):
+#    return string.ascii_lowercase.index('r')
+
+#s = ['P', 'A', 'c', 'R', 'R', 's', 'b']
+
+def swapSort(L): 
+    """ L is a list on integers """
+    print ("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(i+1, len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print (L)
+    print ("Final L: ", L)
+      
