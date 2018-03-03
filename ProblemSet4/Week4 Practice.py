5da4ab9538ac83182f693c79b20377945b612101
Adam Yars <adamyars@outlook.com>
2018-03-02 17:41:10 -0700

    Combined old repos into one folder, deleted duplicates.

---
+++ ProblemSet4/Week4 Practice.py
@@ -0,0 +1,9 @@
+def nfruits(fruitBag, roadFruit):
+    for letter in roadFruit:
+        if letter in fruitBag.keys():
+            fruitBag[letter] -= 1
+    for key in fruitBag.keys():
+        if key not in roadFruit:
+            fruitBag[key] += 1
+
+    return fruitBag
