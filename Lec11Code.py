0700a669d1874367dce657fcfba16c3c95a1bc64
Adam Yars <adamyars@outlook.com>
2018-03-06 06:41:39 -0700

    Lec11 Problem 5

--- Lec11Code.py
+++ Lec11Code.py
@@ -83,27 +83,113 @@ Give the outputs and their type.
 #'''3.12'''
 #print (w2.getY())
 
-'''
-Lecture 11 Problem 4
-Given code below, define two methods for coordinate class:
-    __eq__ : returns true if coordinates same point on plane.
-    __repr__: returns string looks like valid python expression/
-    used to recreate an object with same value.
-   In other words, eval(repr(c)) ==c
-'''
-class Coordinate(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+#'''
+#Lecture 11 Problem 4
+#Given code below, define two methods for coordinate class:
+#    __eq__ : returns true if coordinates same point on plane.
+#    __repr__: returns string looks like valid python expression/
+#    used to recreate an object with same value.
+#   In other words, eval(repr(c)) ==c
+#'''
+#class Coordinate(object):
+#    def __init__(self, x, y):
+#        self.x = x
+#        self.y = y
 
-    def getX(self):
-        # Getter method for a Coordinate object's x coordinate.
-        # Getter methods are better practice than just accessing an attribute directly
-        return self.x
+#    def getX(self):
+#        # Getter method for a Coordinate object's x coordinate.
+#        # Getter methods are better practice than just accessing an attribute directly
+#        return self.x
 
-    def getY(self):
-        # Getter method for a Coordinate object's y coordinate
-        return self.y
+#    def getY(self):
+#        # Getter method for a Coordinate object's y coordinate
+#        return self.y
+
+#    def __eq__(self, other):
+#        if self.x == other.x and self.y == other.y:
+#            return True
+#        else:
+#            return False
+
+#    def __repr__(self):
+#        return 'Coordinate({},{})'.format(self.x, self.y)
+
+
+#    def __str__(self):
+#        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
+#t = Coordinate(10,10)
+#s = Coordinate(1,1)
+#q = Coordinate(1,1)
+
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+        Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+        Raises ValueError if e is not in self
+        self.vals.remove() uses the remove method that is 
+        assosiated with lists. This is different from our 
+        function definition for remove. 
+        """
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def intersect(self, other):
+        intersectList = intSet()
+        for e in self.vals:
+            if e in other.vals:
+                intersectList.insert(e)
+        if len(intersectList) == 0:
+            return print('No elements in common')
+        return intersectList
+
 
     def __str__(self):
-        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
+
+
+
+
+s1 = intSet()
+s = intSet()
+print (s)
+s.insert(3)
+s.insert(4)
+s.insert(6)
+s.insert(7)
+s1.insert(3)
+s1.insert(4)
+s1.insert(6)
+s1.insert(7)
+#s.insert(3)
+#print s
+#s.member(3)
+#s.member(5)
+#s.insert(6)
+#print s
+#s.remove(3)
+#print s
+#s.remove(3)
+
